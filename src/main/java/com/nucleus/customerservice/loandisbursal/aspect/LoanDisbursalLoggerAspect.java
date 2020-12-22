0259b0cba65dbd3f95f2e9fe4e7150cbf188a6eb
Kuldeep Kumar <k2kuldeep@users.noreply.github.com>
2020-12-22 16:47:28 +0530

    logger added in LoanDisbursal

---
+++ src/main/java/com/nucleus/customerservice/loandisbursal/aspect/LoanDisbursalLoggerAspect.java
@@ -0,0 +1,25 @@
+package com.nucleus.customerservice.loandisbursal.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoanDisbursalLoggerAspect {
+
+    Logger LOGGER = LoggerFactory.getLogger(LoanDisbursalLoggerAspect.class);
+
+    @After("execution (* com.nucleus.customerservice.loandisbursal.controller.LoanDisbursalController.getLoanDisbursals(..))")
+    public void fetchbyLonaId(JoinPoint joinPoint) {
+        LOGGER.info("Loan Disbursal Details fetched by LoanApplicationNumber");
+    }
+
+    @After("execution (* com.nucleus.customerservice.loandisbursal.controller.LoanDisbursalController.getLoanDisbursalsByCustomerId(..))")
+    public void fetchbyCustomerId(JoinPoint joinPoint) {
+        LOGGER.info("Loan Disbursal Details fetched by customerCode");
+    }
+}
