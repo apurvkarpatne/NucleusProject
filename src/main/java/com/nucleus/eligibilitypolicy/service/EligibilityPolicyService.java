70246ca0f7d5a6f1dfa2476f47830f1dd1257e81
Kuldeep Kumar <k2kuldeep@users.noreply.github.com>
2020-12-22 19:02:52 +0530

    merge conflicts files

--- src/main/java/com/nucleus/eligibilitypolicy/service/EligibilityPolicyService.java
+++ src/main/java/com/nucleus/eligibilitypolicy/service/EligibilityPolicyService.java
@@ -1,6 +1,7 @@
 package com.nucleus.eligibilitypolicy.service;
 
 import com.nucleus.eligibilitypolicy.model.EligibilityPolicy;
+import com.nucleus.eligibiltyparameter.model.EligibilityParameter;
 
 import java.util.List;
 
@@ -9,7 +10,9 @@ public interface EligibilityPolicyService {
     public List<EligibilityPolicy> getAllEligibilityPolicies();
     public boolean insertEligibilityPolicy(EligibilityPolicy eligibilityPolicy);
     public EligibilityPolicy getOneEligibilityPolicy(String policyCode);
-    public boolean updateStatus(String policyCode, String newStatus, String authorizedBy);
+    public boolean updateStatus(String policyCode, String newStatus);
     public boolean updateEligibilityPolicy(EligibilityPolicy eligibilityPolicy);
     public boolean deleteEligibilityPolicy(String policyCode);
+
+    EligibilityParameter getOneParameterFromName(String parameterName);
 }
--- src/main/java/com/nucleus/eligibilitypolicy/service/EligibilityPolicyServiceImpl.java
+++ src/main/java/com/nucleus/eligibilitypolicy/service/EligibilityPolicyServiceImpl.java
@@ -2,10 +2,10 @@ package com.nucleus.eligibilitypolicy.service;
 
 import com.nucleus.eligibilitypolicy.database.EligibilityPolicyDAO;
 import com.nucleus.eligibilitypolicy.model.EligibilityPolicy;
+import com.nucleus.eligibiltyparameter.model.EligibilityParameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -22,27 +22,23 @@ public class EligibilityPolicyServiceImpl implements EligibilityPolicyService{
         this.eligibilityPolicyDAO = eligibilityPolicyDAO;
     }
 
-    //To get a list of all Eligibility Policies:
     @Override
     public List<EligibilityPolicy> getAllEligibilityPolicies() {
         return eligibilityPolicyDAO.getAllEligibilityPolicies();
     }
 
-    //To add a new Eligibility Policy to database:
     @Override
     public boolean insertEligibilityPolicy(EligibilityPolicy eligibilityPolicy) {
         return eligibilityPolicyDAO.insertEligibilityPolicy(eligibilityPolicy);
     }
 
-    //To retrieve one Eligibility Policy by Policy Code:
     @Override
     public EligibilityPolicy getOneEligibilityPolicy(String policyCode) {
         return eligibilityPolicyDAO.getOneEligibilityPolicy(policyCode);
     }
 
-    //To update status {Approve/Reject} Eligibility Policy:
     @Override
-    public boolean updateStatus(String policyCode, String action, String authorizedBy) {
+    public boolean updateStatus(String policyCode, String action) {
         EligibilityPolicy eligibilityPolicy = eligibilityPolicyDAO.getOneEligibilityPolicy(policyCode);
         String newStatus;
         if(action.equalsIgnoreCase("approve")) {
@@ -52,12 +48,9 @@ public class EligibilityPolicyServiceImpl implements EligibilityPolicyService{
         } else
             newStatus = "PENDING";
         eligibilityPolicy.setStatus(newStatus);
-        eligibilityPolicy.setAuthorizedBy(authorizedBy);
-        eligibilityPolicy.setAuthorizedDate(LocalDate.now());
         return eligibilityPolicyDAO.updateEligibilityPolicy(eligibilityPolicy);
     }
 
-    //To update an existing Eligibility Policy (all fields):
     @Override
     public boolean updateEligibilityPolicy(EligibilityPolicy eligibilityPolicy) {
         EligibilityPolicy oldEligibilityPolicy = eligibilityPolicyDAO.getOneEligibilityPolicy(eligibilityPolicy.getPolicyCode());
@@ -65,17 +58,17 @@ public class EligibilityPolicyServiceImpl implements EligibilityPolicyService{
         oldEligibilityPolicy.setPolicyDescription(eligibilityPolicy.getPolicyDescription());
         oldEligibilityPolicy.setEligibilityParameterList(eligibilityPolicy.getEligibilityParameterList());
         oldEligibilityPolicy.setStatus(eligibilityPolicy.getStatus());
-        oldEligibilityPolicy.setModifiedBy(eligibilityPolicy.getModifiedBy());
-        oldEligibilityPolicy.setModifiedDate(eligibilityPolicy.getModifiedDate());
-        oldEligibilityPolicy.setAuthorizedBy(null);
-        oldEligibilityPolicy.setAuthorizedDate(null);
-        return eligibilityPolicyDAO.updateEligibilityPolicy(oldEligibilityPolicy);
+        return eligibilityPolicyDAO.updateEligibilityPolicy(eligibilityPolicy);
     }
 
-    //To delete an existing Eligibility Policy:
     @Override
     public boolean deleteEligibilityPolicy(String policyCode) {
         return eligibilityPolicyDAO.deleteEligibilityPolicy(policyCode);
+    }
+
+    @Override
+    public EligibilityParameter getOneParameterFromName(String parameterName) {
+        return eligibilityPolicyDAO.getOneParameterFromName(parameterName);
     }
 
 }
